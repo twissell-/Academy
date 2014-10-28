721f85f11d397fbad3332e8dcc0da73519f359f6
Damian Maggio <dmaggioesne@gmail.com>
2014-10-28 17:07:06 -0300

    Agregadas las clases ControllerEspecialidad y COntrollerDocente

---
+++ Bussines/ControllerDocente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Data;
+using Entidades;
+
+namespace Bussines
+{
+    public class ControllerDocente
+    {
+        private DaoDocente dao;
+
+        public ControllerDocente()
+        {
+            dao = new DaoDocente();
+        }
+
+        public Docente find(int id)
+        {
+            return dao.find(id);
+        }
+
+        public List<Docente> find()
+        {
+            return dao.find();
+        }
+
+        public void delete(Docente obj)
+        {
+            dao.delete(obj);
+        }
+
+        public void update(Docente obj)
+        {
+            dao.update(obj);
+        }
+
+        public void insert(Docente obj)
+        {
+            dao.insert(obj);
+        }
+    }
+}
---
+++ Bussines/ControllerEspecialidad.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Data;
+using Entidades;
+
+namespace Bussines
+{
+    public class ControllerEspecialidad
+    {
+        private DaoEspecialidad dao;
+
+        public ControllerEspecialidad()
+        {
+            dao = new DaoEspecialidad();
+        }
+
+        public Especialidad find(int id)
+        {
+            return dao.find(id);
+        }
+
+        public List<Especialidad> find()
+        {
+            return dao.find();
+        }
+
+        public void delete(Especialidad obj)
+        {
+            dao.delete(obj);
+        }
+
+        public void update(Especialidad obj)
+        {
+            dao.update(obj);
+        }
+
+        public void insert(Especialidad obj)
+        {
+            dao.insert(obj);
+        }
+    }
+}
