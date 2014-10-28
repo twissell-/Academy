afce4864ba484d1f93ffa5cccc7e96df7d9a87e9
Damian Maggio <dmaggioesne@gmail.com>
2014-10-28 17:09:32 -0300

    Agregada la clase ControllerPlan

---
+++ Bussines/ControllerPlan.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Data;
+using Entidades;
+
+namespace Bussines
+{
+    public class ControllerPlan
+    {
+        private DaoPlan dao;
+
+        public ControllerPlan()
+        {
+            dao = new DaoPlan();
+        }
+
+        public Plan find(int id)
+        {
+            return dao.find(id);
+        }
+
+        public List<Plan> find()
+        {
+            return dao.find();
+        }
+
+        public void delete(Plan obj)
+        {
+            dao.delete(obj);
+        }
+
+        public void update(Plan obj)
+        {
+            dao.update(obj);
+        }
+
+        public void insert(Plan obj)
+        {
+            dao.insert(obj);
+        }
+    }
+}
