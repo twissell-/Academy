e78fc21b26f279d35a0d33425cadcb3be3f32abd
Damian Maggio <dmaggioesne@gmail.com>
2014-10-28 17:02:02 -0300

    Agregada la clase ControllerComision

---
+++ Bussines/ControllerComision.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Data;
+using Entidades;
+
+namespace Bussines
+{
+    public class ControllerComision
+    {
+        private DaoComision dao;
+
+        public ControllerComision()
+        {
+            dao = new DaoComision();
+        }
+
+        public Comision find(int id)
+        {
+            return dao.find(id);
+        }
+
+        public List<Comision> find()
+        {
+            return dao.find();
+        }
+
+        public void delete(Comision obj)
+        {
+            dao.delete(obj);
+        }
+
+        public void update(Comision obj)
+        {
+            dao.update(obj);
+        }
+
+        public void insert(Comision obj)
+        {
+            dao.insert(obj);
+        }
+    }
+}
