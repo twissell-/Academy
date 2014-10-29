7944c7027a639e0758debcec8afd9155cdd59e70
Damian Maggio <dmaggioesne@gmail.com>
2014-10-29 15:48:06 -0300

    Agregados metoos para el manejo de ids autoincrementales. No terminados

---
+++ Data/DaoParameters.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using Entidades;
+using MongoDB.Driver;
+
+namespace Data
+{
+    public class DaoParameter
+    {
+        // Singleton
+        private static DaoParameter Instance;
+        MongoCollection Parameters;
+
+        private DaoParameter()
+        {
+            MongoServer server = Connection.instance.server;
+            MongoDatabase database = server.GetDatabase("test");
+            Parameters = database.GetCollection<Param>("test");
+        }
+
+        public static DaoParameter instance
+        {
+            get
+            {
+                if (Instance == null)
+                {
+                    Instance = new DaoParameter();
+                }
+                return Instance;
+            }
+        }
+
+        private int getParameterValue(string id)
+        {
+            Param p = this.find("AlumnoNextId");
+            int val = p.value;
+            p.value += 1;
+            this.save(p);
+
+            return val;
+        }
+
+        #region Interfaces para los distintos parametros
+
+        public int getAlumnoNextId()
+        {
+            return getParameterValue("alumnoNextId");
+        }
+
+        public int getComisionNextId()
+        {
+            return getParameterValue("comisionNextId");
+        }
+
+        public int getDocenteNextId()
+        {
+            return getParameterValue("docenteNextId");
+        }
+
+        public int getEspecialidadNextId()
+        {
+            return getParameterValue("especialidadNextId");
+        }
+
+        public int getMateriaNextId()
+        {
+            return getParameterValue("materiaNextId");
+        }
+
+        public int getPlanNextId()
+        {
+            return getParameterValue("planNextId");
+        }
+
+        #endregion
+
+        private void save(Param obj)
+        {
+            Parameters.Save(obj);
+        }
+
+        public Param find(string id)
+        {
+            QueryDocument query = new QueryDocument("_id", id);
+            return Parameters.FindOneAs<Param>(query);
+        }
+
+        public void delete(Param obj)
+        {
+            QueryDocument query = new QueryDocument("_id", obj.id);
+            Parameters.Remove(query);
+        }
+
+        public void update(Param obj)
+        {
+            save(obj);
+        }
+
+        public void insert(Param obj)
+        {
+            save(obj);
+        }
+
+    }
+}
---
+++ Entidades/Param.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entidades
+{
+    public class Param
+    {
+        public String id { get; set; };
+        public int value { get; set; };
+
+
+    }
+}
