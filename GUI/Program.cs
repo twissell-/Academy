1e661bcce7cf4de264fd8fea9ff7c880ae0fd089
nickolas20 <nickolas.pereira20@gmail.com>
2015-01-24 18:50:33 -0300

    login redy pal uso

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -15,7 +15,7 @@ namespace GUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmPrincipal0());
+            Application.Run(new FrmLogin());
         }
     }
 }
